febd9cd7611df2387b642ceeb737d7c1782475f9
pointerboy <lukatrbovic@outlook.com>
2019-01-10 11:44:29 +0100

    tick tock, tick tock

--- application.py
+++ application.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 import requests
 import random
+import time
+from timeit import default_timer as timer
 from flask import Flask, render_template
 app = Flask(__name__)
 
@@ -9,12 +11,15 @@ words = open(words_f).read().splitlines()
 
 @app.route('/')
 def index_route():
-  setenc = "Roses are red, violets are blue"
+  start = timer()
+  setenc = "[*] Generated: Roses are red, violets are blue"
 
   for i in range(0, random.randrange(0, 5)):
     rad_index = random.randrange(0, len(words))
     setenc += " " + words[rad_index]
   print(setenc)
+  end = timer()
+  print(int(end-start))
   return render_template("index.html", setenc=setenc)
 
 if __name__ == "__main__":
