2605928ba49fda7f837663947817b4aa0d48a68f
pointerboy <lukatrbovic@outlook.com>
2019-01-10 01:12:43 +0100

    weird flex, works

--- application.py
+++ application.py
@@ -1,15 +1,21 @@
 #!/usr/bin/env python3
 import requests
+import random
 from flask import Flask, render_template
 app = Flask(__name__)
 
+words_f = "/usr/share/dict/words"
+words = open(words_f).read().splitlines()
+
 @app.route('/')
 def index_route():
-  return render_template("index.html")
+  setenc = "Roses are red, violets are blue"
+
+  for i in range(0, random.randrange(0, 5)):
+    rad_index = random.randrange(0, len(words))
+    setenc += " " + words[rad_index]
+  print(setenc)
+  return render_template("index.html", setenc=setenc)
 
 if __name__ == "__main__":
-  print("listing jokes")
-  for joke in jokes_list:
-    print(joke.contents)
-
   app.run(debug=True)
